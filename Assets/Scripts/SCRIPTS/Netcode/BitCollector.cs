d24cb2c49788fed552dbfecc1cca43b403210a43
alexlukens82991 <78564818+alexlukens82991@users.noreply.github.com>
2023-02-04 21:57:40 -0500

    fuckin siiiiick

--- Assets/Scripts/SCRIPTS/Netcode/BitCollector.cs
+++ Assets/Scripts/SCRIPTS/Netcode/BitCollector.cs
@@ -7,9 +7,31 @@ public class BitCollector : NetworkBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        Bit bit = other.gameObject.GetComponent<Bit>();
-        if (bit == null) return;
-        CurrentBits++;
-        bit.gameObject.SetActive(false);
+        if (other.collider.tag.Equals("Bit"))
+        {
+            CurrentBits++;
+
+            NetworkObject bit = other.gameObject.GetComponent<NetworkObject>();
+            DespawnBitServerRpc(bit.NetworkObjectId);
+        }
+        
+    }
+
+    [ServerRpc]
+    private void DespawnBitServerRpc(ulong id)
+    {
+        NetworkObject found = NetworkManager.SpawnManager.SpawnedObjects[id];
+        if (found != null)
+            Destroy(found.gameObject);
+
+        DespawnBitClientRpc(id);
+    }
+
+    [ClientRpc]
+    private void DespawnBitClientRpc(ulong id)
+    {
+        NetworkObject found = NetworkManager.SpawnManager.SpawnedObjects[id];
+        if (found != null)
+            Destroy(found.gameObject);
     }
 }
