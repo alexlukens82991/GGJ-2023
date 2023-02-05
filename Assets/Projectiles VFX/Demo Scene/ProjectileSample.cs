c8df63c6ecf8f308d788bd09bda51f8a19f7291f
alexlukens82991 <78564818+alexlukens82991@users.noreply.github.com>
2023-02-04 23:49:38 -0500

    projectiles

---
+++ Assets/Projectiles VFX/Demo Scene/ProjectileSample.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSample : MonoBehaviour {
+	public GameObject projectile_fx_prefab;
+	public GameObject muzzle_shot_prefab;
+	public GameObject hit_fx_prefab;
+	private GameObject projectile_fx;
+
+	void Start () {
+		projectile_fx = Instantiate(projectile_fx_prefab, transform.position, transform.rotation);
+		projectile_fx.transform.parent = transform;
+		if(muzzle_shot_prefab != null){
+			GameObject muzzle = Instantiate(muzzle_shot_prefab, transform.position, transform.rotation);
+			muzzle.transform.rotation = transform.rotation;
+			Destroy(muzzle.gameObject, 0.45f);
+		}
+	}
+	
+	void OnCollisionEnter(Collision hit)
+	{
+		//Give the trail a little time to dissipate, then destroy 
+		//this is not necesary but it looks better than if you just destroy the entire vfx at once.
+		int children_count = projectile_fx.transform.childCount;
+		for (int i = 0; i < children_count; i++) {
+			Transform c_aux = projectile_fx.transform.GetChild (0);
+			if(c_aux.name.Contains("trail")){
+				c_aux.transform.parent = null;
+				Destroy(c_aux.gameObject, 2.0f);
+			}
+		}
+		//Instantiate & destroy hit vfx
+		if(hit_fx_prefab != null){
+			GameObject hit_vfx = Instantiate(hit_fx_prefab, transform.position, Quaternion.identity);
+			Destroy(hit_vfx, 0.7f);
+		}
+		//Destroy the prjectile vfx
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Projectiles VFX/Demo Scene/SceneManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+	public float projectile_speed = 1100;
+	public Transform fps_view;
+	public Transform top_view;
+	public Transform cam_view;
+	public TextMesh text_fx_name;
+	public GameObject[] fx_prefabs;
+	public int index_fx = 0;
+
+
+	private Ray ray;
+	private RaycastHit ray_cast_hit;
+
+	void Start () {
+		text_fx_name.text = "[" + (index_fx + 1) + "] " + fx_prefabs[ index_fx ].name;
+		Destroy(GameObject.Find("Instructions"), 12.5f);
+	}
+	
+	void Update () {
+		//Instantiate Projectiles
+		if ( Input.GetMouseButtonDown(0) ){
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if ( Physics.Raycast (ray.origin, ray.direction, out ray_cast_hit, 1000f) ){				
+				GameObject projectile = Instantiate(fx_prefabs[ index_fx ], transform.position, Quaternion.identity);	
+				projectile.transform.LookAt(ray_cast_hit.point);
+				projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * projectile_speed);
+			}
+		}
+		//Change Camera View
+		if ( Input.GetKeyDown("1") ){
+			cam_view.position = fps_view.position;
+			cam_view.rotation = fps_view.rotation;
+		}
+
+		if ( Input.GetKeyDown("2") ){
+			cam_view.position = top_view.position;
+			cam_view.rotation = top_view.rotation;
+		}
+		//Change-FX Keyboard	
+		if ( Input.GetKeyDown("z") || Input.GetKeyDown("left") ){
+			index_fx--;
+			if(index_fx <= -1)
+				index_fx = fx_prefabs.Length - 1;
+			text_fx_name.text = "[" + (index_fx + 1) + "] " + fx_prefabs[ index_fx ].name;	
+		}
+
+		if ( Input.GetKeyDown("x") || Input.GetKeyDown("right")){
+			index_fx++;
+			if(index_fx >= fx_prefabs.Length)
+				index_fx = 0;
+			text_fx_name.text = "[" + (index_fx + 1) + "] " + fx_prefabs[ index_fx ].name;
+		}
+	}
+
+
+}
